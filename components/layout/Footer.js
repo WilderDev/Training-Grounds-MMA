fe611de27291f7c6e2ae75dc5539aa855de3f2e7
William Wilder <WilliamJohnWilder@outlook.com>
2021-09-23 06:43:39 -0500

    ADD: Layout

---
+++ components/layout/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p className="mt-8 text-center text-base text-gray-400">
+        &copy; {new Date().getFullYear()} — Training Grounds, All Rights
+        Reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
