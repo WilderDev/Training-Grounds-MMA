69659d21b7a976a7de42d9142db6a1f001b60504
William Wilder <WilliamJohnWilder@outlook.com>
2021-10-29 08:04:30 -0500

    CREATE: Extra Details Context

---
+++ contexts/ExtraDetails.context.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useMemo, useState } from "react";
+
+const ExtraDetailsCtx = createContext();
+
+function ExtraDetailsProvider({ children }) {
+  const [numFighters, setNumFighters] = useState(1);
+  // TSK: More Dtails
+
+  const value = useMemo(() => ({
+    numFighters,
+    setNumFighters,
+  }));
+
+  return (
+    <ExtraDetailsCtx.Provider value={value}>
+      {children}
+    </ExtraDetailsCtx.Provider>
+  );
+}
+
+function useExtraDetails() {
+  const ctx = useContext(ExtraDetailsCtx);
+
+  if (ctx === undefined)
+    throw new Error("useExtraDetails must be used with a ExtraDetailsProvider");
+
+  return ctx;
+}
+
+export { ExtraDetailsProvider, useExtraDetails };
