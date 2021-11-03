da18713c2dcced9cbbaccd2888018fcc164ed360
William Wilder <WilliamJohnWilder@outlook.com>
2021-11-03 08:07:27 -0500

    Working Version w/ MongoDB Adapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,27 +2,35 @@ import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
 
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import { connectToDatabase } from "../../../utils/auth.helpers";
+import clientPromise from "../../../utils/auth.helpers";
 
-export default NextAuth({
-  providers: [
-    EmailProvider({
-      server: {
-        host: "",
-        port: "",
-        auth: {
-          user: "",
-          password: "",
-        },
-      },
-      from: process.env.EMAIL_FROM,
+// export default NextAuth({
+//   providers: [
+//     EmailProvider({
+//       server: process.env.EMAIL_SERVER,
+//       from: process.env.EMAIL_FROM,
+//     }),
+//     //     IX_TSK: Add more providers here: https://next-auth.js.org/getting-started/example
+//     // Facebook & Google.
+//   ],
+//   database: process.env.DATABASE_URL,
+//   adapter: MongoDBAdapter({
+//     db: await connectToDatabase(),
+//   }),
+//   secret: process.env.AUTH_SECRET,
+// });
+
+export default async function auth(req, res) {
+  return await NextAuth(req, res, {
+    adapter: MongoDBAdapter({
+      db: (await clientPromise).db("Users"),
     }),
-    //     IX_TSK: Add more providers here: https://next-auth.js.org/getting-started/example
-    // Facebook & Google.
-  ],
-  database: process.env.DATABASE_URL,
-  adapter: MongoDBAdapter({
-    db: connectToDatabase(),
-  }),
-  secret: process.env.AUTH_SECRET,
-});
+    providers: [
+      EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+      }),
+      //     IX_TSK: Add Facebook & Google providers
+    ],
+  });
+}
--- utils/auth.helpers.js
+++ utils/auth.helpers.js
@@ -1,25 +1,33 @@
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_AUTH_URI;
-
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 };
-let cachedClient = null;
-let cachedDB = null;
 
-if (!uri) throw new Error("Please deine the MONGODB_AUTH_URI Env-Var");
+let client;
+let clientPromise;
 
-export async function connectToDatabase() {
-  if (cachedClient && cachedDB) return { client: cachedClient, db: cachedDB };
-
-  const client = await MongoClient.connect(uri, options);
-
-  const db = client.db();
-
-  cachedClient = client;
-  cachedDB = db;
-
-  return { client, db };
+if (!process.env.MONGODB_URI) {
+  throw new Error("Please add your Mongo Auth URI to .env.local");
 }
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
